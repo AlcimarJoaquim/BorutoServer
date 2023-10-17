e4b0da55575bede1b973835f1e777523d90ce4cd
Alcimar <alcimarjoaquim@outlook.com>
2023-10-16 23:33:27 -0300

    Testing root endpoint

--- src/test/kotlin/com/example/ApplicationTest.kt
+++ src/test/kotlin/com/example/ApplicationTest.kt
@@ -4,18 +4,18 @@ import com.example.plugins.*
 import io.ktor.client.request.*
 import io.ktor.client.statement.*
 import io.ktor.http.*
+import io.ktor.server.application.*
 import io.ktor.server.testing.*
 import kotlin.test.*
 
 class ApplicationTest {
     @Test
-    fun testRoot() = testApplication {
-        application {
-            configureRouting()
-        }
-        client.get("/").apply {
-            assertEquals(HttpStatusCode.OK, status)
-            assertEquals("Hello World!", bodyAsText())
+    fun testRoot() {
+        withTestApplication(moduleFunction = Application::module){
+            handleRequest(HttpMethod.Get, "/").apply {
+                assertEquals(expected = HttpStatusCode.OK, actual = response.status())
+                assertEquals("Welcome to Boruto API!", response.content)
+            }
         }
     }
 }
